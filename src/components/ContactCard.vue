eb58efbfe80b3f31998f918187313b5c1366f6e1
LeVietNhut <lnhut2410@gmail.com>
2023-03-03 20:12:58 +0700

    hien thi danh sach contact lay ve tu server

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: {
+        contact: { type: Object, required: true },
+    },
+    // mounted() {
+    //     console.log(this.contact);
+    // }
+};
+</script>
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Tên:</strong>
+            {{contact.name }}
+        </div>
+        <div class="p-1">
+            <strong>E-mail:</strong>
+            {{ contact.email }}
+        </div>
+        <div class="p-1">
+            <strong>Địa chỉ:</strong>
+            {{ contact.address }}
+        </div>
+        <div class="p-1">
+            <strong>Điện thoại:</strong>
+            {{ contact.phone }}
+        </div>
+    <div class="p-1 d-flex">
+            <strong>Liên hệ yêu thích:&nbsp;</strong>
+            <!-- {{ contact.favorite ? ` <i class="fas fa-check"></i>` : `s` }} -->
+            <div v-if="contact.favorite == true">
+                <i class="fas fa-check"></i>
+            </div>
+            <div v-else>
+                <i class="fas fa-times"></i>
+            </div>
+            <!-- <i v-if="this.contact.favorite == true" class="fas fa-check"></i>
+                <i v-else class="fas fa-times"></i> -->
+        </div>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "./router";
 
-import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
-createApp(App).mount('#app')
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { far } from '@fortawesome/free-regular-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// library.add(fab, fas, far)
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+import './assets/main.css';
+
+import './assets/form.css';
+
+createApp(App).use(router).mount('#app')
